b64bb1b9c701f3792e2c36571f896f1cf4c4ce39
Hbkalrz09 <haibibie26@gmail.com>
2025-03-16 14:38:18 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website is loaded!");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function() {
+        navMenu.classList.toggle("show");
+    });
 });
