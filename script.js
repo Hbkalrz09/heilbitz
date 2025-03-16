3a506a7ee8ee8bccd1ae204513badb02ca313978
Hbkalrz09 <haibibie26@gmail.com>
2025-03-16 14:44:39 +0700

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
-
-    menuToggle.addEventListener("click", function() {
-        navMenu.classList.toggle("show");
-    });
-});
+function toggleMenu() {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.toggle("show");
+}
